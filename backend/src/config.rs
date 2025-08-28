42c8985260eb87176ca61e907dd557507b6e000c
0x5b62656e5d <salt@pepper.fyi>
2025-08-28 11:14:38 +0800

    Add SMTP env vars

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -5,6 +5,10 @@ use std::sync::LazyLock;
 pub struct Env {
     pub port: u16,
     pub db_url: String,
+    pub smtp_host: String,
+    pub smtp_port: u16,
+    pub smtp_user: String,
+    pub smtp_password: String,
 }
 
 impl Env {
@@ -23,6 +27,10 @@ impl Env {
         Self {
             port: Self::get_env("PORT").parse().unwrap(),
             db_url: Self::get_env("DATABASE_URL"),
+            smtp_host: Self::get_env("SMTP_HOST"),
+            smtp_port: Self::get_env("SMTP_PORT").parse().unwrap(),
+            smtp_user: Self::get_env("SMTP_USER"),
+            smtp_password: Self::get_env("SMTP_PASSWORD"),
         }
     }
 }
