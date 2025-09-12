83b9712388a93ab10d9d8dfc617eff2fc978af68
0x5b62656e5d <salt@pepper.fyi>
2025-09-12 20:42:47 +0800

    chore: add lint command

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +9,39 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+    {
+        ignores: ["**/node_modules/**", "**/.next/**", "**/build/**", "next-env.d.ts"],
+    },
+    ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:import/recommended"),
+    {
+        settings: {
+            "import/resolver": {
+                typescript: true,
+            },
+        },
+        rules: {
+            "import/order": [
+                "error",
+                {
+                    groups: [["builtin", "external", "internal"]],
+                    pathGroups: [
+                        {
+                            pattern: "react",
+                            group: "external",
+                            position: "before",
+                        },
+                    ],
+                    pathGroupsExcludedImportTypes: ["builtin"],
+                    alphabetize: {
+                        order: "asc",
+                        caseInsensitive: true,
+                    },
+                    "newlines-between": "always",
+                },
+            ],
+        },
+    },
+];
 
 export default eslintConfig;
