0f029aad8044d2f3f6a25d143ba82829a0054175
0x5b62656e5d <salt@pepper.fyi>
2025-09-13 15:01:26 +0800

    feat: add nextjs api routes for comment/delete

---
+++ frontend/src/app/api/url/comment/[id]/route.ts
@@ -0,0 +1,55 @@
+import api from "@/util/api";
+import StandardResponse from "@/util/types";
+import { isAxiosError } from "axios";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = await params;
+
+    try {
+        const cookieValue = request.cookies.get("__Host-access") as RequestCookie;
+
+        await api.patch(
+            `/url/comment/${id}`,
+            { ...(await request.json()) },
+            {
+                headers: {
+                    Cookie: `__Host-access=${cookieValue?.value ?? ""}`,
+                },
+            },
+        );
+
+        const response: StandardResponse = {
+            success: true,
+            message: null,
+            data: null,
+            error: null,
+        };
+
+        return NextResponse.json(response, { status: 200 });
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            console.error("Error response from server:", error.response.data);
+            const response: StandardResponse = {
+                success: false,
+                message: null,
+                data: null,
+                error:
+                    error.response.data.error ||
+                    "An error occurred while updating the comment of the URL",
+            };
+
+            return NextResponse.json(response, { status: error.response.status });
+        } else {
+            console.error("Unexpected error:", error);
+            const response: StandardResponse = {
+                success: false,
+                message: null,
+                data: null,
+                error: "An unexpected error occurred while updating the comment of the URL",
+            };
+            return NextResponse.json(response, { status: 500 });
+        }
+    }
+}
